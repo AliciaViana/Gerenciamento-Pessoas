cb1ce60cb23c54e7a152591a546d812efd35ee9f
AliciaViana <aliciasouza601@gmail.com>
2022-03-06 11:43:52 -0300

    refatorar codigo; close #1

--- src/com/numberone/Gerenciador.java
+++ src/com/numberone/Gerenciador.java
@@ -2,31 +2,22 @@ package com.numberone;
 
 import java.util.Scanner;
 
-public class Abadois {
+public class Gerenciador {
 
 	public static void main(String[] args) {
 		Pessoa novaPessoa = new Pessoa();
  		PessoaServico pessoaServico = new PessoaServico();
 		Scanner myObj = new Scanner(System.in);
 		
-		
 		System.out.println("Diggite seu nome:  ");
 		String nome = myObj.nextLine();
-		
-		
 		System.out.println("digite cor do seu cabelo: ");
 		String corCabelo = myObj.nextLine();
 		
-
-
 		novaPessoa.setNome(nome);
 		novaPessoa.setCorCabelo(corCabelo);
 		
 		pessoaServico.carregaNome(novaPessoa.getNome());
-		
 		pessoaServico.carregaCorCabelo(novaPessoa.getCorCabelo());
-		
-
 	}
-	
 }
--- src/com/numberone/PessoaServico.java
+++ src/com/numberone/PessoaServico.java
@@ -1,22 +1,17 @@
 package com.numberone;
 
 public class PessoaServico {
-	
-	public void carregaNome (String nome) {
-		System.out.println("Seu nome é: " + nome); 
-		
+
+	public void carregaNome(String nome) {
+		System.out.println("Seu nome é: " + nome);
 	}
-	public void carregaIdade (int idade) {
-		
+
+	public void carregaIdade(int idade) {
 		int anoNascimento = 2022 - idade;
 		System.out.println("o ano que voce nasceu é " + anoNascimento);
-		
-		
 	}
-	public void carregaCorCabelo (String corCabelo) {
-		System.out.println("cor do seu cabelo é: " + corCabelo);
-		
-	}
-	
 
+	public void carregaCorCabelo(String corCabelo) {
+		System.out.println("cor do seu cabelo é: " + corCabelo);
+	}
 }
