0bf5469a712d3ac2b88e2aa3aa6d6f4dde1acbb2
AliciaViana <aliciasouza601@gmail.com>
2022-03-05 21:37:11 -0300

    Meu primeiro commit

---
+++ src/com/numberone/Pessoa.java
@@ -0,0 +1,20 @@
+package com.numberone;
+
+public class Pessoa {
+	private String nome;
+	private String corCabelo;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCorCabelo() {
+		return corCabelo;
+	}
+	public void setCorCabelo(String corCabelo) {
+		this.corCabelo = corCabelo;
+	}
+	
+}
---
+++ src/com/numberone/PessoaServico.java
@@ -0,0 +1,22 @@
+package com.numberone;
+
+public class PessoaServico {
+	
+	public void carregaNome (String nome) {
+		System.out.println("Seu nome é: " + nome); 
+		
+	}
+	public void carregaIdade (int idade) {
+		
+		int anoNascimento = 2022 - idade;
+		System.out.println("o ano que voce nasceu é " + anoNascimento);
+		
+		
+	}
+	public void carregaCorCabelo (String corCabelo) {
+		System.out.println("cor do seu cabelo é: " + corCabelo);
+		
+	}
+	
+
+}
